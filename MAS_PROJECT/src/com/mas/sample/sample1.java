3c10f19ebe2b97f33890687e492613fcd97917d4
rakotapa <ravikanth.kotapati@capgemini.com>
2017-10-30 12:39:45 -0400

    Commit 2

--- MAS_PROJECT/src/com/mas/sample/sample1.java
+++ MAS_PROJECT/src/com/mas/sample/sample1.java
@@ -3,7 +3,7 @@ package com.mas.sample;
 public class sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test commit");
+		System.out.println("Test commit2");
 		// TODO Auto-generated method stub
 
 	}
