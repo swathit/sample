859f596429e5fbef07399ee9e176d972c46f6b53
ravikanth.kotapati@capgemini.com <ravikanth.kotapati@capgemini.com>
2017-10-30 11:54:46 -0400

    second commit

--- MAS_PROJECT/src/com/mas/sample/sample1.java
+++ MAS_PROJECT/src/com/mas/sample/sample1.java
@@ -5,6 +5,8 @@ public class sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to MAS");
+		System.out.println("Welcome to MAS TEAM");
+		
 
 	}
 
